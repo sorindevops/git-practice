db30cddc202f2a8821868ca71b8be7d58cc54e30
Diana Soriano <dssori@gmail.com>
2022-09-23 16:58:59 -0400

    refactored

--- function_c.py
+++ function_c.py
@@ -3,12 +3,8 @@ def merge_lists(list_a, list_b):
         a combination of list_a and list_b
         without any duplicate elements.
     """
-    list_a = set(list_a)
-    list_b = set(list_b)
-    list_c = list_a | list_b
-    return list(list_c)
-    
-
+    return list(set(list_a) | set(list_b))
+# refactored code to this one line 
 
 if __name__ == "__main__":
     print(merge_lists([1, 1, 2, 3], [3, 4, 5]))
